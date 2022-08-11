7211eda15e7c17caef6be0e4951afeac2b45fe44
Chris Wedgwood <cw@f00f.org>
2022-08-11 08:37:20 -0700

    count /proc/cpuinfo

--- n.go
+++ n.go
@@ -3,11 +3,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"runtime"
 )
+
+func cpuinfoCount() (ncpu int) {
+	d, _ := ioutil.ReadFile("/proc/cpuinfo")
+	for _, l := range bytes.Split(d, []byte("\n")) {
+		if bytes.HasPrefix(l, []byte("processor")) {
+			ncpu++
+		}
+	}
+	return
+}
 
 func main() {
 	fmt.Println("runtime.NumCPU():      ", runtime.NumCPU())
 	fmt.Println("runtime.GOMAXPROCS(0): ", runtime.GOMAXPROCS(0))
+	fmt.Println("/proc/cpuinfo (count): ", cpuinfoCount())
 }
